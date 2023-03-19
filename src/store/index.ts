f75e6385d110fbdc09259a763279b29e03246744
ccchdx <2919129325@qq.com>
2023-03-19 21:54:15 +0800

    feat(main): vuex的ts技巧和菜单栏展示

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
-import { createStore } from 'vuex'
+import { createStore, Store, useStore as useVuexStore } from 'vuex'
 
 import login from './login/login'
 
-import { IRootState } from './types'
+import { IRootState, IStoreType } from './types'
 
 const store = createStore<IRootState>({
   state() {
@@ -22,6 +22,11 @@ const store = createStore<IRootState>({
 //登录后请求的数据存储在vuex和本地，但当页面发生刷新时，存储在vuex中的请求数据会消失.故通过编写函数setupStore将本地将数据重新存储到vuex，在main.ts中调用，每次刷新时都会执行
 export function setupStore() {
   store.dispatch('login/loadLocalLogin')
+}
+
+//定义login子模块的自己useStore,做类型限制
+export function useStore(): Store<IStoreType> {
+  return useVuexStore()
 }
 
 export default store
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,12 @@
+import { ILoginState } from './login/types'
+
 export interface IRootState {
   name: string
   age: number
 }
+
+//定义login子模块自己的useStore的类型
+export interface IRootWithModule {
+  login: ILoginState
+}
+export type IStoreType = IRootState & IRootWithModule

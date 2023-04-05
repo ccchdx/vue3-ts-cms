7cf8a3492ee0e363531c55a15a48e9aed3ea7dcc
ccchdx <2919129325@qq.com>
2023-04-06 02:59:04 +0800

    feat(user): hyTable动态插槽和作用域插槽

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
-    <div class="content"></div>
+    <div class="content">
+      <hy-table :listData="userList" :propList="propList">
+        <template #status="scope">
+          <el-button>{{ scope.row.enable ? '启用' : '禁用' }}</el-button>
+        </template>
+        <template #createAt="scope">
+          <strong>{{ scope.row.createAt }}</strong>
+        </template>
+      </hy-table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 import PageSearch from '@/components/page-search'
+import HyTable from '@/base-ui/table'
 
 import { useStore } from 'vuex'
 
@@ -16,9 +26,11 @@ import { searchFormConfig } from './config/search.config'
 export default defineComponent({
   name: 'user',
   components: {
-    PageSearch
+    PageSearch,
+    HyTable
   },
   setup() {
+    //发送用户列表请求
     const store = useStore()
     store.dispatch('system/getPageListAction', {
       pageUrl: '/users/list',
@@ -27,11 +39,42 @@ export default defineComponent({
         size: 10
       }
     })
+
+    const userList = computed(() => store.state.system.userList)
+    const userCount = computed(() => store.state.system.userCount)
+    //propList中的数据(表头数据)为userList中数据的改写
+    const propList = [
+      { prop: 'name', label: '用户名', minWidth: '100' },
+      { prop: 'realname', label: '真实姓名', minWidth: '100' },
+      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+      { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
+      {
+        prop: 'createAt',
+        label: '创建时间',
+        minWidth: '250',
+        slotName: 'createAt'
+      },
+      {
+        prop: 'updateAt',
+        label: '更新时间',
+        minWidth: '250',
+        slotName: 'updateAt'
+      },
+      { label: '操作', minWidth: '120', slotName: 'handler' }
+    ]
     return {
-      searchFormConfig
+      searchFormConfig,
+      userList,
+      userCount,
+      propList
     }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
